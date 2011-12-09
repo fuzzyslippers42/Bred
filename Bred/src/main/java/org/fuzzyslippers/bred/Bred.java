2b412d72912d206745fabf2a1914e1cecf122d78
fuzzyslippers42 <fuzzyslipppers42@gmail.com>
2011-12-08 21:33:03 -0500

    Adding Maven Files and what not.

---
+++ Bred/src/main/java/org/fuzzyslippers/bred/Bred.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fuzzyslippers.bred;
+
+/**
+ *
+ * @author Nick
+ */
+public class Bred {
+    
+}
