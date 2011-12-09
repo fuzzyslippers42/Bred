d6532791044f80dab3115c2ba27098efe407afa2
fuzzyslippers42 <fuzzyslipppers42@gmail.com>
2011-12-08 22:29:23 -0500

    Added the required jedis dependencies. Also adding bukkit dependencies. Also adding other things. That is all.

--- Bred/src/main/java/org/fuzzyslippers/bred/Bred.java
+++ Bred/src/main/java/org/fuzzyslippers/bred/Bred.java
@@ -3,12 +3,20 @@
  * and open the template in the editor.
  */
 package org.fuzzyslippers.bred;
-
+import redis.clients.jedis.*;
+import redis.clients.jedis.Jedis;
 
 /**
  *
  * @author Nick
  */
 public class Bred {
-    
+    JedisPool jPool = new JedisPool(new JedisPoolConfig(), "localhost");
+    Jedis jedis = new jPool.getResource();
+    static public boolean makeConnection(String databaseName){
+        // This function creates the database connection with the database named databaseName
+        jedis.set("server:name",databaseName);
+        
+        return true;
+    }
 }
